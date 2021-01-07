222472033cff2c5159ed98bfa85a7dcaf855dd64
Lausen <liuxiang@52toys.com>
2021-01-07 15:09:50 +0800

    商城配置编写

--- flutter_shop/lib/config/font.dart
+++ flutter_shop/lib/config/font.dart
@@ -1,1 +1,9 @@
- 
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class KFont{
+  static const TextStyle PRICE_STYLE = TextStyle(
+    color: Colors.black26,
+    decoration: TextDecoration.lineThrough,
+  );
+}
