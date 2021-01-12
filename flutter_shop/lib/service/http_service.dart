c6906ed73d5c16ca722ec87c1ffb0eddb3803222
Lausen <liuxiang@52toys.com>
2021-01-12 10:55:08 +0800

    利用了HttpClient封装了get/post请求

--- flutter_shop/lib/service/http_service.dart
+++ flutter_shop/lib/service/http_service.dart
@@ -13,10 +13,9 @@ class HttpService{
 
     try{
       httpClient = HttpClient();
-      request = await httpClient.getUrl(Uri.parse(url));
-
+      request = await httpClient.postUrl(Uri.parse(url));
       //设置请求头
-
+      await setHttpHead(request);
       //设置请求参数
       if (params != null){
         request.add(utf8.encode(json.encode(params)));
@@ -35,5 +34,50 @@ class HttpService{
       }
     }
     return null;
+  }
+
+
+  //get请求
+  static Future<Map<String,dynamic>> get(String url,{Map<String,dynamic> params}) async {
+    //设置get请求的请求头
+    if(url.indexOf('?') < 0){
+      url += "?";
+    }
+    if(params != null){
+      params.forEach((key, value) {
+        url += '$key=${value.toString()}&';
+      });
+    }
+    url = url.substring(0,url.length - 1);
+
+    HttpClient httpClient;
+    HttpClientRequest request;
+    HttpClientResponse response;
+
+    try{
+      httpClient = HttpClient();
+      request = await httpClient.getUrl(Uri.parse(url));
+      //设置请求头
+      await setHttpHead(request);
+      response = await request.close();
+      String body = await response.transform(utf8.decoder).join();
+      if(response.statusCode == 200){
+        Map<String,dynamic> resBody = json.decode(body);
+        return resBody;
+      }else{
+        print('请求失败....');
+      }
+    }finally{
+      if(request != null){
+        request.close();
+      }
+    }
+    return null;
+  }
+
+  static void setHttpHead(HttpClientRequest request) async {
+    request.headers.set(HttpHeaders.contentTypeHeader, 'application/json;charset=UTF-8');
+    String token = await TokenUtil.getToken();
+    request.headers.set('Authorization', token);
   }
 }
