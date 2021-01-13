a2598b7c2592cf17cfce5186f4f507c198fa1c03
Lausen <liuxiang@52toys.com>
2021-01-13 10:01:38 +0800

    做好了首页数据的请求,开始布局首页

--- flutter_shop/lib/page/home/home_page.dart
+++ flutter_shop/lib/page/home/home_page.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/config/index.dart';
 import 'package:flutter_shop/page/home/home_banner.dart';
 import 'package:flutter_shop/page/home/home_category.dart';
 import 'package:flutter_shop/page/home/home_good.dart';
 import 'package:flutter_shop/model/home_content_model.dart';
+import 'package:flutter_shop/service/http_service.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -21,7 +24,17 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin 
   @override
   void initState() {
     super.initState();
-    print('首页刷新');
+    _initData();
+  }
+
+  _initData() async{
+    var response = await HttpService.get(ApiUrl.HOME_CONTENT);
+    this.setState(() {
+      _homeModel = HomeContentModel.fromJson(response['data']);
+      _homeModel.goods.forEach((element) {
+        print(element.name);
+      });
+    });
   }
 
   @override
@@ -29,14 +42,20 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin 
     super.build(context);
     return Scaffold(
       backgroundColor: Color.fromRGBO(245, 245, 245, 1.0),
-      appBar: AppBar(title: Text(KString.HOME_TITLE),),
-      body: ListView(
-        children: [
-          HomeBanner(_homeModel.banners),
-          HomeCategory(_homeModel.category),
-          HomeGood(_homeModel.goods),
-        ],
+      appBar: AppBar(
+        title: Text(KString.HOME_TITLE),
       ),
+      body: _homeModel == null
+          ? Center(
+              child: Text('暂无数据...'),
+            )
+          : ListView(
+              children: [
+                HomeBanner(_homeModel.banners),
+                HomeCategory(_homeModel.category),
+                HomeGood(_homeModel.goods),
+              ],
+            ),
     );
   }
 }
