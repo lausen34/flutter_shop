2ade3b2e111584785f395c3c0be140d7b8d9fedf
Lausen <liuxiang@52toys.com>
2021-01-13 10:16:49 +0800

    做好了首页轮播图的数据赋值

--- flutter_shop/lib/page/home/home_banner.dart
+++ flutter_shop/lib/page/home/home_banner.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_shop/model/home_content_model.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomeBanner extends StatelessWidget {
-
   List<HomeBannerModel> _banners;
 
   HomeBanner(this._banners);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.red,
+      height: ScreenUtil().setHeight(333),
+      width: ScreenUtil().setWidth(750),
+      child: Swiper(
+        itemCount: _banners.length,
+        itemBuilder: (context, index) {
+          return Image.network(
+            _banners[index].image,
+            fit: BoxFit.cover,
+          );
+        },
+        pagination: SwiperPagination(),
+        autoplay: true,
+      ),
+    );
   }
 }
