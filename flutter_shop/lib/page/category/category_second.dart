51b37fb8c6d7ad7440e6f0cac93786ac17193007
Lausen <liuxiang@52toys.com>
2021-01-14 13:44:55 +0800

    二级分类的数据赋值和界面布局

--- flutter_shop/lib/page/category/category_second.dart
+++ flutter_shop/lib/page/category/category_second.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_shop/call/call.dart';
 import 'package:flutter_shop/model/category_model.dart';
 import 'package:flutter_shop/call/notifiy.dart';
@@ -11,7 +12,6 @@ class CategorySecond extends StatefulWidget {
 }
 
 class _CategorySecondState extends State<CategorySecond> {
-
   List<CategoryModel> _secondList = [];
   int _firstCategoryId = 0;
   int _secondCategoryId = 0;
@@ -22,20 +22,21 @@ class _CategorySecondState extends State<CategorySecond> {
     Call.addCallBack(Notify.REFRESH_SECOND_CATEGORY, _refreshCategory);
   }
 
-  void _refreshCategory(data){
+  void _refreshCategory(data) {
     setState(() {
       _firstCategoryId = data['firstCategoryId'];
     });
     _getSecondCategory(data['firstCategoryId']);
   }
-  
-  _getSecondCategory(int id) async{
-    var response = await HttpService.get(ApiUrl.CATEGORY_SECOND,params: {'pid': id});
+
+  _getSecondCategory(int id) async {
+    var response =
+        await HttpService.get(ApiUrl.CATEGORY_SECOND, params: {'pid': id});
     CategoryListModel model = CategoryListModel.fromJson(response['data']);
     model.list.forEach((element) {
       print(element.name);
     });
-    if (model.list.length > 0){
+    if (model.list.length > 0) {
       var secondId = model.list[0].id;
       setState(() {
         _secondList = model.list;
@@ -43,12 +44,70 @@ class _CategorySecondState extends State<CategorySecond> {
       });
     }
   }
-  
+
   @override
   Widget build(BuildContext context) {
-
     return Container(
-      child: Text('二级分类'),
+      padding: EdgeInsets.only(left: 10),
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+              top: 20.0,
+            ),
+          ),
+
+          GridView.builder(
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: _secondList.length,
+            shrinkWrap: true,
+            padding: EdgeInsets.all(4.0),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 20.0,
+              mainAxisSpacing: 20.0,
+            ),
+            itemBuilder: (context,index){
+                return _categoryItem(_secondList[index]);
+            },
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: 20,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _categoryItem(CategoryModel categoryModel) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _secondCategoryId = categoryModel.id;
+        });
+        //TODO 二级分类的点击事件
+      },
+      child: Container(
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            Image.network(
+              categoryModel.image,
+              fit: BoxFit.cover,
+              height: 60,
+            ),
+            Text(
+              categoryModel.name,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.black54,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
