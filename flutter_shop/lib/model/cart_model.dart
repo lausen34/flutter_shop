038ceaacf71812397758ab292383644c8c0bb913
Lausen <liuxiang@52toys.com>
2021-01-20 10:59:46 +0800

    购物车网络请求

--- flutter_shop/lib/model/cart_model.dart
+++ flutter_shop/lib/model/cart_model.dart
@@ -1,3 +1,66 @@
-class CartModel{
+class CartListModel{
+  List<CartModel> list;
+  CartListModel(this.list);
 
+  CartListModel.fromJson(Map<String,dynamic> json){
+    if(json['list'] != null){
+      list = List<CartModel>();
+      json['list'].forEach((v){
+        list.add(CartModel.fromJson(v));
+      });
+    }
+  }
+
+  Map<String,dynamic> toJson(){
+    final Map<String,dynamic> data = Map<String,dynamic>();
+    if(this.list != null){
+      data['list'] = this.list.map((e) => e.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class CartModel {
+  int id;
+  int user_id;
+  int good_id;
+  int good_count;
+  String good_name;
+  int good_price;
+  String good_image;
+  int is_checked;
+
+  CartModel(
+    this.id,
+    this.user_id,
+    this.good_count,
+    this.good_name,
+    this.good_price,
+    this.good_image,
+    this.is_checked,
+  );
+
+  CartModel.fromJson(Map<String,dynamic> json){
+    this.id = json['id'];
+    this.user_id = json['user_id'];
+    this.good_id = json['good_id'];
+    this.good_count = json['good_count'];
+    this.good_name = json['good_name'];
+    this.good_price = json['good_price'];
+    this.good_image = json['good_image'];
+    this.is_checked = json['is_checked'];
+  }
+
+  Map<String,dynamic> toJson(){
+    final Map<String,dynamic> data = Map<String,dynamic>();
+    data['id'] = this.id;
+    data['user_id'] = this.user_id;
+    data['good_id'] = this.good_id;
+    data['good_count'] = this.good_count;
+    data['good_name'] = this.good_name;
+    data['good_price'] = this.good_price;
+    data['good_image'] = this.good_image;
+    data['is_checked'] = this.is_checked;
+    return data;
+  }
 }
--- flutter_shop/lib/page/cart/cart_page.dart
+++ flutter_shop/lib/page/cart/cart_page.dart
@@ -1,19 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/component/small_button.dart';
+import 'package:flutter_shop/config/index.dart';
+import 'package:flutter_shop/model/cart_model.dart';
+import 'package:flutter_shop/service/http_service.dart';
+import 'package:flutter_shop/utils/router_util.dart';
+import 'package:flutter_shop/utils/token_util.dart';
+import 'package:flutter_shop/data/data_center.dart';
+
 
 class CartPage extends StatefulWidget {
   @override
   _CartPageState createState() => _CartPageState();
 }
-
 class _CartPageState extends State<CartPage> {
+  bool _refresh = false;
+  bool _isLogin = true;
+  int _userId = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    this._initData();
+  }
+  void _initData() async{
+    var user = await TokenUtil.getUserInfo();
+    this.setState(() {
+      _userId = user.id;
+    });
+    var params = {
+      'user_id':_userId,
+    };
+    var response = await HttpService.get(ApiUrl.CART_LIST,params: params);
+    if(response != null && response['code'] == 0){
+      CartListModel model = CartListModel.fromJson(response['data']);
+      DataCenter.getInstance().cartList = model.list;
+      this.setState(() {
+        _refresh = !_refresh;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('购物车'),
+        title: Text(KString.CART_TITLE),
       ),
-      body: Center(
-        child: Text('购物车'),
+      body: this._isLogin
+          ? Stack(
+              children: [
+                ListView(),
+                Positioned(
+                  left: 0,
+                  bottom: 0,
+                  child: Text('fuck'),
+                ),
+              ],
+            )
+          : Center(
+        child: KSmallButton(
+          text: KString.LOGIN_TITLE,
+          onPressed: (){
+            RouterUtil.toLoginPage(context);
+          },
+        ),
       ),
     );
   }
